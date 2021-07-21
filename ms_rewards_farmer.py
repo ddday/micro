3d628ecb5359c90e7984842a52dead87c0aa9226
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2021-07-21 11:18:58 -0400

    Update ms_rewards_farmer.py
    removed dupe code and consolidated.

--- ms_rewards_farmer.py
+++ ms_rewards_farmer.py
@@ -367,6 +367,16 @@ def bingSearch(browser: WebDriver, word: str, isMobile: bool):
     return points
 
 
+def openCloseWindow(browser: WebDriver, minTime: int = 8, maxTime: int = 17):
+    time.sleep(1)
+    browser.switch_to.window(window_name=browser.window_handles[1])
+    time.sleep(random.randint(minTime, maxTime))
+    browser.close()
+    time.sleep(2)
+    browser.switch_to.window(window_name=browser.window_handles[0])
+    time.sleep(2)
+
+
 def completePromotionalItems(browser: WebDriver):
     try:
         item = getDashboardData(browser)["promotionalItem"]
@@ -378,13 +388,7 @@ def completePromotionalItems(browser: WebDriver):
             browser.find_element_by_xpath(
                 '//*[@id="promo-item"]/section/div/div/div/a'
             ).click()
-            time.sleep(1)
-            browser.switch_to.window(window_name=browser.window_handles[1])
-            time.sleep(8)
-            browser.close()
-            time.sleep(2)
-            browser.switch_to.window(window_name=browser.window_handles[0])
-            time.sleep(2)
+            openCloseWindow(browser, 8, 8)
     except:
         pass
 
@@ -396,13 +400,7 @@ def completeDailySetSearch(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-daily-set-item-content/div/div[3]/a"
     ).click()
-    time.sleep(1)
-    browser.switch_to.window(window_name=browser.window_handles[1])
-    time.sleep(random.randint(13, 17))
-    browser.close()
-    time.sleep(2)
-    browser.switch_to.window(window_name=browser.window_handles[0])
-    time.sleep(2)
+    openCloseWindow(browser, 13, 17)
 
 
 def completeDailySetSurvey(browser: WebDriver, cardNumber: int):
@@ -430,6 +428,9 @@ def completeDailySetQuiz(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-daily-set-item-content/div/div[3]/a"
     ).click()
+
+
+def Generic_Quiz(browser: WebDriver, cardNumber: int):
     time.sleep(1)
     browser.switch_to.window(window_name=browser.window_handles[1])
     time.sleep(8)
@@ -564,6 +565,10 @@ def completeDailySetThisOrThat(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-daily-set-item-content/div/div[3]/a"
     ).click()
+    ThisOrThat(browser, cardNumber)
+
+
+def ThisOrThat(browser: WebDriver, cardNumber: int):
     time.sleep(1)
     browser.switch_to.window(window_name=browser.window_handles[1])
     time.sleep(8)
@@ -690,13 +695,7 @@ def completePunchCard(browser: WebDriver, url: str, childPromotions: Dict[Any, A
                 browser.execute_script(
                     "document.getElementsByClassName('offer-cta')[0].click()"
                 )
-                time.sleep(1)
-                browser.switch_to.window(window_name=browser.window_handles[1])
-                time.sleep(random.randint(13, 17))
-                browser.close()
-                time.sleep(2)
-                browser.switch_to.window(window_name=browser.window_handles[0])
-                time.sleep(2)
+                openCloseWindow(browser, 13, 17)
             if child["promotionType"] == "quiz":
                 browser.execute_script(
                     "document.getElementsByClassName('offer-cta')[0].click()"
@@ -768,13 +767,7 @@ def completeMorePromotionSearch(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-more-activities-card-item/div/div[3]/a"
     ).click()
-    time.sleep(1)
-    browser.switch_to.window(window_name=browser.window_handles[1])
-    time.sleep(random.randint(13, 17))
-    browser.close()
-    time.sleep(2)
-    browser.switch_to.window(window_name=browser.window_handles[0])
-    time.sleep(2)
+    openCloseWindow(browser, 13, 17)
 
 
 def completeMorePromotionQuiz(browser: WebDriver, cardNumber: int):
@@ -783,62 +776,7 @@ def completeMorePromotionQuiz(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-more-activities-card-item/div/div[3]/a"
     ).click()
-    time.sleep(1)
-    browser.switch_to.window(window_name=browser.window_handles[1])
-    time.sleep(8)
-    if not waitUntilQuizLoads(browser):
-        resetTabs(browser)
-        return
-    browser.find_element_by_xpath('//*[@id="rqStartQuiz"]').click()
-    waitUntilVisible(
-        browser, By.XPATH, '//*[@id="currentQuestionContainer"]/div/div[1]', 10
-    )
-    time.sleep(3)
-    numberOfQuestions = browser.execute_script(
-        "return _w.rewardsQuizRenderInfo.maxQuestions"
-    )
-    numberOfOptions = browser.execute_script(
-        "return _w.rewardsQuizRenderInfo.numberOfOptions"
-    )
-    for _ in range(numberOfQuestions):
-        if numberOfOptions == 8:
-            answers = []
-            for i in range(8):
-                if (
-                    browser.find_element_by_id("rqAnswerOption" + str(i))
-                    .get_attribute("iscorrectoption")
-                    .lower()
-                    == "true"
-                ):
-                    answers.append("rqAnswerOption" + str(i))
-            for answer in answers:
-                browser.find_element_by_id(answer).click()
-                time.sleep(5)
-                if not waitUntilQuestionRefresh(browser):
-                    return
-            time.sleep(5)
-        elif numberOfOptions == 4:
-            correctOption = browser.execute_script(
-                "return _w.rewardsQuizRenderInfo.correctAnswer"
-            )
-            for i in range(4):
-                if (
-                    browser.find_element_by_id("rqAnswerOption" + str(i)).get_attribute(
-                        "data-option"
-                    )
-                    == correctOption
-                ):
-                    browser.find_element_by_id("rqAnswerOption" + str(i)).click()
-                    time.sleep(5)
-                    if not waitUntilQuestionRefresh(browser):
-                        return
-                    break
-            time.sleep(5)
-    time.sleep(5)
-    browser.close()
-    time.sleep(2)
-    browser.switch_to.window(window_name=browser.window_handles[0])
-    time.sleep(2)
+    Generic_Quiz(browser, cardNumber)
 
 
 def completeMorePromotionABC(browser: WebDriver, cardNumber: int):
@@ -884,44 +822,7 @@ def completeMorePromotionThisOrThat(browser: WebDriver, cardNumber: int):
         + str(cardNumber)
         + "]/div/card-content/mee-rewards-more-activities-card-item/div/div[3]/a"
     ).click()
-    time.sleep(1)
-    browser.switch_to.window(window_name=browser.window_handles[1])
-    time.sleep(8)
-    if not waitUntilQuizLoads(browser):
-        resetTabs(browser)
-        return
-    browser.find_element_by_xpath('//*[@id="rqStartQuiz"]').click()
-    waitUntilVisible(
-        browser, By.XPATH, '//*[@id="currentQuestionContainer"]/div/div[1]', 10
-    )
-    time.sleep(3)
-    for _ in range(10):
-        answerEncodeKey = browser.execute_script("return _G.IG")
-
-        answer1 = browser.find_element_by_id("rqAnswerOption0")
-        answer1Title = answer1.get_attribute("data-option")
-        answer1Code = getAnswerCode(answerEncodeKey, answer1Title)
-
-        answer2 = browser.find_element_by_id("rqAnswerOption1")
-        answer2Title = answer2.get_attribute("data-option")
-        answer2Code = getAnswerCode(answerEncodeKey, answer2Title)
-
-        correctAnswerCode = browser.execute_script(
-            "return _w.rewardsQuizRenderInfo.correctAnswer"
-        )
-
-        if answer1Code == correctAnswerCode:
-            answer1.click()
-            time.sleep(8)
-        elif answer2Code == correctAnswerCode:
-            answer2.click()
-            time.sleep(8)
-
-    time.sleep(5)
-    browser.close()
-    time.sleep(2)
-    browser.switch_to.window(window_name=browser.window_handles[0])
-    time.sleep(2)
+    ThisOrThat(browser, cardNumber)
 
 
 def completeMorePromotions(browser: WebDriver):
